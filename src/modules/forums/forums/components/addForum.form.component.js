9cd7f559676cebb05bfa65227fd7bd57478d29ec
Alexandru <ionutbudisteanu@yahoo.com>
2017-05-25 02:32:57 +0300

    addForum form

--- src/modules/forums/forums/components/addForum.form.component.js
+++ src/modules/forums/forums/components/addForum.form.component.js
@@ -31,6 +31,8 @@ import {
     Panel,
     Button,
     PanelBody,
+    PanelHeader,
+    PanelFooter,
     FormGroup,
     InputGroup,
     HelpBlock,
@@ -78,7 +80,7 @@ export class AddForumForm extends React.Component {
         if (error === "notEmpty") return "It's empty"
     }
 
-    handleCheckRegister(e){
+    handleAddForum(e){
 
         e.preventDefault(); e.stopPropagation();
 
@@ -86,53 +88,53 @@ export class AddForumForm extends React.Component {
         var onError = this.props.onError || function (){};
 
         /*
-        var userNameValidationStatus = [null, ''],  emailAddressValidationStatus = [null, ''],  firstNameValidationStatus = [null, ''], lastNameValidationStatus = [null, ''], passwordValidationStatus = [null,  ''],
-            retypePasswordValidationStatus = [null,  ''], countryValidationStatus = [null,  ''],  cityValidationStatus = [null,  ''];
+         var userNameValidationStatus = [null, ''],  emailAddressValidationStatus = [null, ''],  firstNameValidationStatus = [null, ''], lastNameValidationStatus = [null, ''], passwordValidationStatus = [null,  ''],
+         retypePasswordValidationStatus = [null,  ''], countryValidationStatus = [null,  ''],  cityValidationStatus = [null,  ''];
 
-        var bValidationError = false;
+         var bValidationError = false;
 
-        if (this.state.password.length < 4){
-            passwordValidationStatus = ["error","To weak. At least 4 characters"];
-            bValidationError = true;
-        }
+         if (this.state.password.length < 4){
+         passwordValidationStatus = ["error","To weak. At least 4 characters"];
+         bValidationError = true;
+         }
 
-        if ((this.state.password !== this.state.retypePassword)&&(this.state.password !== '')){
-            retypePasswordValidationStatus = ["error","The passwords don't match"];
-            bValidationError = true;
-        }
+         if ((this.state.password !== this.state.retypePassword)&&(this.state.password !== '')){
+         retypePasswordValidationStatus = ["error","The passwords don't match"];
+         bValidationError = true;
+         }
 
-        this.setState({
-            userNameValidationStatus : userNameValidationStatus, emailAddressValidationStatus : emailAddressValidationStatus,
-            firstNameValidationStatus : firstNameValidationStatus, lastNameValidationStatus : lastNameValidationStatus,
-            passwordValidationStatus : passwordValidationStatus, retypePasswordValidationStatus : retypePasswordValidationStatus,
-            countryValidationStatus : countryValidationStatus, cityValidationStatus : cityValidationStatus,
-        });
+         this.setState({
+         userNameValidationStatus : userNameValidationStatus, emailAddressValidationStatus : emailAddressValidationStatus,
+         firstNameValidationStatus : firstNameValidationStatus, lastNameValidationStatus : lastNameValidationStatus,
+         passwordValidationStatus : passwordValidationStatus, retypePasswordValidationStatus : retypePasswordValidationStatus,
+         countryValidationStatus : countryValidationStatus, cityValidationStatus : cityValidationStatus,
+         });
 
 
-        if (!bValidationError)
-            this.AuthService.registerAsync(this.state.userName, this.state.emailAddress, this.state.password, this.state.firstName, this.state.lastName, this.state.countryCode, '', this.state.city, this.state.latitude, this.state.longitude, this.state.timeZone)
+         if (!bValidationError)
+         this.AuthService.registerAsync(this.state.userName, this.state.emailAddress, this.state.password, this.state.firstName, this.state.lastName, this.state.countryCode, '', this.state.city, this.state.latitude, this.state.longitude, this.state.timeZone)
 
-                .then( (res) =>{
+         .then( (res) =>{
 
-                    console.log(res);
+         console.log(res);
 
-                    if (res.result === "true") onSuccess(res);
-                    else
-                    if (res.result === "false"){
+         if (res.result === "true") onSuccess(res);
+         else
+         if (res.result === "false"){
 
-                        if ((typeof res.errors.username !=="undefined")&&(Object.keys(res.errors.username).length !== 0 )) this.setState({userNameValidationStatus : ["error", this.convertValidationErrorToString(res.errors.username[0])]});
-                        if ((typeof res.errors.email !=="undefined")&&(Object.keys(res.errors.email).length !== 0)) this.setState({emailAddressValidationStatus : ["error", this.convertValidationErrorToString(res.errors.email[0])]});
-                        if ((typeof res.errors.firstName !=="undefined")&&(Object.keys(res.errors.firstName).length !== 0)) this.setState({firstNameValidationStatus : ["error", this.convertValidationErrorToString(res.errors.firstName[0])]});
-                        if ((typeof res.errors.lastName !=="undefined")&&(Object.keys(res.errors.lastName).length  !== 0)) this.setState({lastNameValidationStatus : ["error", this.convertValidationErrorToString(res.errors.lastName[0])]});
-                        if ((typeof res.errors.country !=="undefined")&&(Object.keys(res.errors.country).length  !== 0)) this.setState({countryValidationStatus : ["error", this.convertValidationErrorToString(res.errors.country[0])]});
-                        if ((typeof res.errors.city !=="undefined")&&(Object.keys(res.errors.city).length  !== 0)) this.setState({cityValidationStatus : ["error", this.convertValidationErrorToString(res.errors.city[0])]});
+         if ((typeof res.errors.username !=="undefined")&&(Object.keys(res.errors.username).length !== 0 )) this.setState({userNameValidationStatus : ["error", this.convertValidationErrorToString(res.errors.username[0])]});
+         if ((typeof res.errors.email !=="undefined")&&(Object.keys(res.errors.email).length !== 0)) this.setState({emailAddressValidationStatus : ["error", this.convertValidationErrorToString(res.errors.email[0])]});
+         if ((typeof res.errors.firstName !=="undefined")&&(Object.keys(res.errors.firstName).length !== 0)) this.setState({firstNameValidationStatus : ["error", this.convertValidationErrorToString(res.errors.firstName[0])]});
+         if ((typeof res.errors.lastName !=="undefined")&&(Object.keys(res.errors.lastName).length  !== 0)) this.setState({lastNameValidationStatus : ["error", this.convertValidationErrorToString(res.errors.lastName[0])]});
+         if ((typeof res.errors.country !=="undefined")&&(Object.keys(res.errors.country).length  !== 0)) this.setState({countryValidationStatus : ["error", this.convertValidationErrorToString(res.errors.country[0])]});
+         if ((typeof res.errors.city !=="undefined")&&(Object.keys(res.errors.city).length  !== 0)) this.setState({cityValidationStatus : ["error", this.convertValidationErrorToString(res.errors.city[0])]});
 
-                        onError(res);
-                    }
+         onError(res);
+         }
 
-                });
+         });
 
-        */
+         */
     }
 
     componentDidMount() {
@@ -196,18 +198,27 @@ export class AddForumForm extends React.Component {
             <PanelContainer controls={false} style={{marginBottom:20, marginTop:20}}>
 
                 <Panel>
+
+                    <PanelHeader className='bg-blue' style={{textAlign: "center"}}>
+                        <Grid>
+                            <Row>
+                                <Col xs={12} className='fg-white'>
+                                    <h3>Create a <strong>Forum</strong></h3>
+                                </Col>
+                            </Row>
+                        </Grid>
+                    </PanelHeader>
+
                     <PanelBody style={{padding: 0}}>
-                        <div className='text-center'>
-                            <h3 style={{margin: 0, padding: 20}}> Create a  <strong>Forum</strong> </h3>
-                        </div>
-                        <div>
-                            <div style={{padding: 25, paddingTop: 0, paddingBottom: 0, margin: 'auto', marginBottom: 25, marginTop: 25}}>
+
+                        <div style={{padding: 25, paddingTop: 0, paddingBottom: 0, margin: 'auto', marginBottom: 25, marginTop: 25}}>
 
 
-                                <Form onSubmit={::this.handleCheckRegister}>
+                            <Form onSubmit={::this.handleAddForum}>
 
+                                <Row>
                                     <FormGroup controlId='titleInput' validationState={this.state.titleValidationStatus[0]}>
-                                        <InputGroup style={{marginRight: 10}}>
+                                        <InputGroup>
                                             <InputGroup.Addon>
                                                 <Icon glyph='icon-fontello-pen' />
                                             </InputGroup.Addon>
@@ -217,67 +228,68 @@ export class AddForumForm extends React.Component {
                                         <HelpBlock>{this.state.titleValidationStatus[1]}</HelpBlock>
                                     </FormGroup>
 
+                                </Row>
+
+                                <Row>
                                     <FormGroup controlId='descriptionInput' validationState={this.state.descriptionValidationStatus[0]}>
                                         <InputGroup >
                                             <InputGroup.Addon>
                                                 <Icon glyph='icon-fontello-mail' />
                                             </InputGroup.Addon>
-                                            <FormControl type='text'  className='border-focus-blue' placeholder='description' value={this.state.description} onChange={::this.handleDescriptionChange} />
+                                            <FormControl type='text'  componentClass='textarea' rows='3' className='border-focus-blue' placeholder='description' value={this.state.description} onChange={::this.handleDescriptionChange} />
                                             <FormControl.Feedback />
                                         </InputGroup>
                                         <HelpBlock>{this.state.descriptionValidationStatus[1]}</HelpBlock>
                                     </FormGroup>
 
+                                </Row>
 
-                                    <Row>
-                                        <Col xs={6} collapseLeft collapseRight >
-                                            <FormGroup controlId='country' validationState={this.state.countryValidationStatus[0]}>
-                                                <InputGroup style={{marginRight: 10}}>
-                                                    <InputGroup.Addon>
-                                                        <Icon glyph='icon-fontello-flag-1' />
-                                                    </InputGroup.Addon>
 
-                                                    <CountrySelect controlId="countrySelect" multi={false} flagImagePath="/../../imgs/app/flags/flags/flat/flagicons/"  ref={(input) => this.countryInput = input}  value={this.state.countryCode}  onSelect={this.handleCountrySelect} />
-                                                    <FormControl.Feedback />
-                                                </InputGroup>
-                                                <HelpBlock>{this.state.countryValidationStatus[1]}</HelpBlock>
-                                            </FormGroup>
-                                        </Col>
-                                        <Col xs={6} collapseLeft collapseRight >
-                                            <FormGroup controlId='city' validationState={this.state.cityValidationStatus[0]}>
-                                                <InputGroup >
-                                                    <InputGroup.Addon>
-                                                        <Icon glyph='icon-fontello-home-1' />
-                                                    </InputGroup.Addon>
-                                                    <FormControl type='city' className='border-focus-blue' placeholder='city'  value={this.state.city} onChange={::this.handleCityChange} />
-                                                    <FormControl.Feedback />
-                                                </InputGroup>
-                                                <HelpBlock>{this.state.cityValidationStatus[1]}</HelpBlock>
-                                            </FormGroup>
-                                        </Col>
-                                    </Row>
+                                <Row>
+                                    <Col xs={6} collapseLeft collapseRight >
+                                        <FormGroup controlId='country' validationState={this.state.countryValidationStatus[0]}>
+                                            <InputGroup style={{marginRight: 10}}>
+                                                <InputGroup.Addon>
+                                                    <Icon glyph='icon-fontello-flag-1' />
+                                                </InputGroup.Addon>
 
-                                    <FormGroup>
-                                        <Grid>
-                                            <Row>
-                                                <Col xs={6} collapseLeft collapseRight style={{paddingTop: 10}}>
+                                                <CountrySelect controlId="countrySelect" multi={false} flagImagePath="/../../imgs/app/flags/flags/flat/flagicons/"  ref={(input) => this.countryInput = input}  value={this.state.countryCode}  onSelect={this.handleCountrySelect} />
+                                                <FormControl.Feedback />
+                                            </InputGroup>
+                                            <HelpBlock>{this.state.countryValidationStatus[1]}</HelpBlock>
+                                        </FormGroup>
+                                    </Col>
+                                    <Col xs={6} collapseLeft collapseRight >
+                                        <FormGroup controlId='city' validationState={this.state.cityValidationStatus[0]}>
+                                            <InputGroup >
+                                                <InputGroup.Addon>
+                                                    <Icon glyph='icon-fontello-home-1' />
+                                                </InputGroup.Addon>
+                                                <FormControl type='city' className='border-focus-blue' placeholder='city'  value={this.state.city} onChange={::this.handleCityChange} />
+                                                <FormControl.Feedback />
+                                            </InputGroup>
+                                            <HelpBlock>{this.state.cityValidationStatus[1]}</HelpBlock>
+                                        </FormGroup>
+                                    </Col>
+                                </Row>
 
-                                                    <div>
-                                                        <Link to={getPath(this,'login')} onClick = {onSwitch.bind(this)}> <strong> Login </strong></Link>to SkyHub
-                                                    </div>
 
-                                                </Col>
-                                                <Col xs={6} collapseLeft collapseRight className='text-right'>
-                                                    <Button lg type='submit' bsStyle='primary' onClick={::this.handleCheckRegister}>Register</Button>
-                                                </Col>
-                                            </Row>
-                                        </Grid>
-                                    </FormGroup>
-                                </Form>
-                            </div>
-
+                            </Form>
                         </div>
                     </PanelBody>
+
+                    <PanelFooter className='bg-lightblue'>
+
+                        <Grid>
+                            <Row>
+                                <Col className='text-right' style={{paddingTop:20, paddingBottom:20, paddingRight:20}}>
+                                    <Button lg type='submit' bsStyle='primary' onClick={::this.handleAddForum}>Create Forum</Button>
+                                </Col>
+                            </Row>
+                        </Grid>
+
+                    </PanelFooter>
+
                 </Panel>
             </PanelContainer>
         );
